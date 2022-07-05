5150342421915b5868489a910e5ec1d0034d3ffd
IrwanS99 <irwansanjaya7999@gmail.com>
2022-07-05 12:17:25 +0700

    Tantangan: Pengenalan React
    React Componen menampilkan dara Contact.json

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <></>
+        <div className='header'>
+            <h1>Call A Friend</h1>
+            <h3>Your Friendly Contact App</h3>
+            <div className='dot'>
+                <span>.......................................................................</span>
+            </div>
+        </div>
     )
 }
 
